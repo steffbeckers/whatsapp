cc18d49d75866e03ccb4b06a5dc8f5d7c70249df
Steff Beckers <steff@steffbeckers.com>
2025-10-24 20:40:40 +0200

    Main updates

--- main.js
+++ main.js
@@ -13,8 +13,29 @@ const client = new Client({
 });
 
 // When the client is ready, run this code (only once)
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("Client is ready!");
+
+  // Get all chats
+  const chats = await client.getChats();
+
+  // Filter individual chats
+  const individuals = chats.filter((chat) => !chat.isGroup);
+
+  console.log(`Found ${individuals.length} individuals:`);
+  individuals.forEach((individual) => {
+    console.log(`- ${individual.name} (${individual.id._serialized})`);
+  });
+
+  // Filter group chats
+  const groups = chats.filter((chat) => chat.isGroup);
+
+  console.log(`Found ${groups.length} groups:`);
+  groups.forEach((group) => {
+    console.log(`- ${group.name} (${group.id._serialized})`);
+  });
+
+  await client.sendMessage("32499765192@c.us", "Cosy WhatsApp Poll Sender is ready!");
 });
 
 // When the client received QR-Code
